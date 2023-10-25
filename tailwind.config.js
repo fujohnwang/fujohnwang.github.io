b612053d51e5fbab737aaddd1e7218277298a21b
afoo <i@afoo.me>
2023-10-25 15:18:46 +0800

    packup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,41 @@
+// Modify your tailwind.config.js <- https://www.skcript.com/blog/disable-tailwind-prose-code
+const disabledCss = {
+	'code::before': false,
+	'code::after': false,
+	'blockquote p:first-of-type::before': false,
+	'blockquote p:last-of-type::after': false,
+	pre: false,
+	code: false,
+	'pre code': false,
+	'code::before': false,
+	'code::after': false,
+}
+
 module.exports = {
     darkMode: 'class',
     content: ['./**/*.html','./templates/**/*.html'],
     plugins: [require('@tailwindcss/typography'),require("daisyui")],
+    theme: {
+      extend: {
+        // typography: {
+        //     DEFAULT: {
+        //         css: {
+        //             "code::before": {content: ''},
+        //             "code::after": {content: ''},
+        //         }
+        //     }
+        // }
+        typography: {
+          DEFAULT: { css: disabledCss },
+          sm: { css: disabledCss },
+          lg: { css: disabledCss },
+          xl: { css: disabledCss },
+          '2xl': { css: disabledCss },
+          'dark': { css: disabledCss },
+          '*': { css: disabledCss },
+        },
+      }
+    },
     daisyui: {
       styled: true,
       themes: true,
