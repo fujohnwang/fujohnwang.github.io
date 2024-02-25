2abf9dc7999c1a912a0d063ec640bf3ee5ef99f8
afoo <i@afoo.me>
2024-02-25 22:56:44 +0800

    packup

--- js/countdown.js
+++ js/countdown.js
@@ -1,32 +1,36 @@
 window.endTimestamp = null;
 
 function startCountDown(minutes) {
-    const now = new Date();
-    now.setMinutes(now.getMinutes() + minutes);
-    window.endTimestamp = now;
+  let min = 1;
+  if (minutes) {
+    min = minutes;
+  }
+  const now = new Date();
+  now.setMinutes(now.getMinutes() + minutes);
+  window.endTimestamp = now;
 
-    updateCountdown();
+  updateCountdown();
 }
 function updateCountdown() {
-    const currentDate = new Date();
-    const timeDifference = window.endTimestamp - currentDate;
-    // console.log(`now: ${currentDate}`)
-    // console.log(`diff: ${timeDifference}`)
-    // console.log(`end: ${window.endTimestamp}`)
+  const currentDate = new Date();
+  const timeDifference = window.endTimestamp - currentDate;
+  // console.log(`now: ${currentDate}`)
+  // console.log(`diff: ${timeDifference}`)
+  // console.log(`end: ${window.endTimestamp}`)
 
-    // 如果倒计时已结束，则停止更新
-    if (timeDifference <= 0) {
-      document.getElementById('countdown').innerHTML = '倒计时结束';
-      return;
-    }
-  
-    // 计算倒计时的分钟和秒
-    const minutes = Math.floor(timeDifference / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  // 如果倒计时已结束，则停止更新
+  if (timeDifference <= 0) {
+    document.getElementById('countdown').innerHTML = '倒计时结束';
+    return;
+  }
 
-    // 更新倒计时显示
-    document.getElementById('countdown').innerHTML = `${minutes} 分钟 ${seconds} 秒`;
-  
-    // 每秒更新一次倒计时
-    setTimeout(updateCountdown, 1000);
+  // 计算倒计时的分钟和秒
+  const minutes = Math.floor(timeDifference / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  // 更新倒计时显示
+  document.getElementById('countdown').innerHTML = `${minutes} 分钟 ${seconds} 秒`;
+
+  // 每秒更新一次倒计时
+  setTimeout(updateCountdown, 1000);
 }
